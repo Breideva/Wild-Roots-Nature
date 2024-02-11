b9d6a162579bddbc2a00924ea4412eca24ff870e
Evan Breidecker <125213012+Breideva@users.noreply.github.com>
2024-02-11 13:11:31 -0600

    Started revising Body

--- Wild-Roots-Nature/src/components/Body.tsx
+++ Wild-Roots-Nature/src/components/Body.tsx
@@ -1,9 +1,31 @@
-
 function Body() {
-    return (
-        <>
-        </>
-    )
+  return (
+    <div>
+      <div className=" p-10 bg-green-700">
+        <div>
+          <h1 className="text-[70px] font-extrabold justify-start mt-5 ml-20">
+            What we do
+          </h1>
+          <div className=" justify-center text-center mt-10 text-[25px]">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero
+              odit ex, unde facere sed laborum repellat minus veritatis, est cum
+              porro. Fugiat amet rem vel corrupti, tenetur aperiam saepe nostrum
+              suscipit nesciunt eaque laborum, earum harum consectetur voluptas
+              natus cum corporis nisi voluptatem veniam laudantium dolore vitae
+              temporibus quis illo? Vero quisquam iusto ipsam deserunt, eum
+              assumenda facilis animi perferendis commodi aliquid delectus
+              dolores ipsa a nihil tenetur qui alias numquam mollitia repellat
+              cum voluptatibus! Vel laborum rerum, autem repellat non corrupti
+              soluta neque quibusdam dignissimos nulla voluptates quia numquam
+              id laboriosam labore quas ab accusamus corporis ducimus aspernatur
+              ad.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Body
+export default Body;
